a8f94c523b3d47bd060f36f8cdb8c3b298db3e64
M-Pestka <michal.pestka1@gmail.com>
2019-12-10 22:02:07 +0100

    comments and docs

---
+++ dokumentacja?.md
@@ -0,0 +1,81 @@
+# Projekt AAL
+
+Michał Pestka
+
+## Opis problemu
+
+(Numer zadania 9)
+
+Treść zadania: 
+
+> Tutaj idzie treść zadania. 
+
+Problem postawiony w zadaniu polega więc na stworzeniu specyficznego rodzaju drzewa rozpinającego. 
+
+**Klasyczne drzewo rozpinające** 
+Problem drzewa rozpinającego polega na znalezieniu sieci połączneń nie zawierającej cykli która łączy wszystkie wierzchołki, dodatkowo spełniając pewnie kryterium.  Zwykle szukamy drzewa o jaknajniższej sumie wag krawędzi.
+
+**Nasze drzewo rozpinające**
+W zadaniu chodzi o znalezienie drzewa rozpinającego które maksymalizuje wagę krawędzi o najniższej wadze. 
+
+**Algorytm Kruskala**
+
+Okazuje się że Algorytm kruskala, który służy do znajdowania klasycznego drzewa rozpinającego, rozwiązuje problem dokładnie odwrotny do zadanego. Wystarczy więc zamienić znajdowanie najtańszej krawędzi na szukanie najdroższej.
+
+## Metody rozwiązania
+
+Rozwiązanie zostało napisane w języku Python3. Dzieli się na 2 części:
+
+* Cześć zawieracjąca algorytm
+* Część zawierająca algorytm
+
+### Algorytm rozwiązania
+
+Zgodnie z tym, co zostało napisane powyżej, użyłem niznacznie zmodyfikowanego algorytmu Kruskala.
+
+**Algorytm Kruskala**
+Algortm Kruskala jest bardzo prosty. Kroki algorytmu:
+
+* Utworzenie lasu (zbioru drzew) w którym każdy z wierzchołków jest osobnym drzewem.
+* Iteracja po zbiorze krawędzi zaczynając od krawędzi o najmniejszej wadze:
+  * Wybranie pierwszej krawędzi oraz usunięcie jej ze zbioru krawędzi.
+  * Sprawdznie do ilu drzew należy krawędź:
+    * Jeśli do 1 -> odrzucamy krawędź.
+    * Jeśli do 2 -> łączymy te dwa drzewa w jedno oraz dołączamy krawędź.
+  * Wyjdź z pętli jeśli istnieje tylko jedno drzewo (jest ono rozpinające)
+
+**Mój Algorytm**
+
+Nie różni się zbytnio od wyżej wymienionego algorytmu. Jedne różnice to odwrócenie sortowania oraz dynamiczne dodawanie drzew kiedy zajdzie taka potrzeba.
+
+Kroki algorytmu:
+
+* Utworzenie pustej listy przeznaczonej na drzewa.
+* Iteracja po zbiorze krawędzi zaczynając od krawędzi o najmniejszej wadze:
+  * Wybranie pierwszej krawędzi oraz usunięcie jej ze zbioru krawędzi.
+  * Sprawdznie do ilu drzew należy krawędź:
+    * Jeśli do 0 -> tworzymy nowe drzewo zawierające 2 wierzchołki krawędzi oraz dodajemy je do listy drzew.
+    * Jeśli do 1 -> odrzucamy krawędź.
+    * Jeśli do 2 -> łączymy te dwa drzewa w jedno oraz dołączamy krawędź.
+
+
+
+
+
+### Algorytmy generatora danych
+
+Powstały dwa generatory danych. 
+
+**Generator1**
+Zadaniem generatora 1 jest stworzenie zadania o zadanych parametrach którego wynik znamy. 
+
+
+
+## Analiza złożoności
+
+## Testowanie
+
+
+
+## Badanie 
+
--- podejscie_1.py
+++ podejscie_1.py
@@ -1,7 +1,11 @@
 import sys
 import time
 
+
 def read_graph():
+    """
+    fukncja wczytująca graf ze standardowego wejścia
+    """
     edges = []
     for l in sys.stdin:
         src, tgt, w = [x.strip() for x in l.split(',')]
@@ -9,14 +13,20 @@ def read_graph():
 
     return edges
 
-def sort_graph(edges):
-    return sorted(edges, key = lambda x: x[2], reverse = True)
 
-#! /bin/python3
+def sort_graph(edges):
+    """
+    sortowanie grafu
+
+    :param edges: list(tuple()) lista zawierająca krotki opisujące poszczególne połącznia w grafie.
+    """
+    return sorted(edges, key=lambda x: x[2], reverse=True)
+
+
+# ! /bin/python3
 
 def main(timeit, edges):
-    
-
+    # do pomiaru czasu wykonania
     t = time.time()
     edges = sort_graph(edges)
 
@@ -27,53 +37,53 @@ def main(timeit, edges):
         set_number_v1 = -1
         set_number_v2 = -1
         for i, st in enumerate(subtrees):
+            # sprawdzenie czy któryś z wierzchołków należących do krawędzie znajduje się w którym z poddrzew
             v1 = e[0] in st
             v2 = e[1] in st
-            if(v1 and v2):
-                # byłby cykl w grafie
+            if (v1 and v2):
+                # nic nie robimy bo byłby cykl w grafie
                 new_subtree = False
                 break
-            
-            if(v1):
+
+            if (v1):
                 # dołączmy tę krawędź do istniejącego drzewa
                 st.add(e[1])
                 set_number_v1 = i
                 new_subtree = False
                 final_edges.append(e)
-                
 
-            if(v2):
+            if (v2):
                 # dołączmy tę krawędź do istniejącego drzewa
                 st.add(e[0])
                 set_number_v2 = i
                 new_subtree = False
                 final_edges.append(e)
-                
-            
+
             # lecimy w pętli
 
         # tworzymy nowe poddrzewo
-        if(new_subtree):
+        if (new_subtree):
             final_edges.append(e)
             subtrees.append(set(e[:2]))
 
         else:
             # sprawdzamy czy należy połączyć 2 zbiory wierzchołków
-            if(set_number_v1 != -1 and set_number_v2 != -1):
+            if (set_number_v1 != -1 and set_number_v2 != -1):
                 subtrees[set_number_v1] = subtrees[set_number_v1].union(subtrees[set_number_v2])
                 subtrees.remove(subtrees[set_number_v2])
-    if(len(subtrees) != 1):
+    if (len(subtrees) != 1):
         raise Exception('Nie da się znaleźć wartości maksymalnej')
-    if(timeit):
+    if (timeit):
+        # pomiar czasu
         return time.time() - t
     else:
         return min([x[2] for x in final_edges])
 
 
-if(__name__ == '__main__'):
+if (__name__ == '__main__'):
     edges = read_graph()
-    if(len(sys.argv) > 1 and sys.argv[1] == 'timeit'):
-        rval = main(timeit = True, edges= edges)
+    if (len(sys.argv) > 1 and sys.argv[1] == 'timeit'):
+        rval = main(timeit=True, edges=edges)
     else:
-        rval = main(timeit = False, edges = edges)
+        rval = main(timeit=False, edges=edges)
     print(rval)
